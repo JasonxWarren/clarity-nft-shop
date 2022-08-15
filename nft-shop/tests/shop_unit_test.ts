069a4d157f9c5af44739b16154e6a9af22cab4ed
JasonxWarren <warrenjason1@gmail.com>
2022-08-15 02:44:12 -0400

    helper function for minting

--- nft-shop/tests/shop_unit_test.ts
+++ nft-shop/tests/shop_unit_test.ts
@@ -6,3 +6,24 @@ const contractName = 'shop';
 const defaultNftAssetContract = 'sip009-nft-trait';
 const defaultPaymentAssetContract = 'sip010-ft-trait';
 
+const contractPrincipal = (deployer: Account) => `${deployer.address}.${contractName}`;
+
+function mintNft({ chain, deployer, recipient, nftAssetContract = defaultNftAssetContract }: { chain: Chain, deployer: Account, recipient: Account, nftAssetContract?: string }) {
+    const block = chain.mineBlock([
+        Tx.contractCall(nftAssetContract, 'mint', [types.principal(recipient.address)], deployer.address),
+    ]);
+    block.receipts[0].result.expectOk();
+    const nftMintEvent = block.receipts[0].events[0].nft_mint_event;
+    const [nftAssetContractPrincipal, nftAssetId] = nftMintEvent.asset_identifier.split('::');
+    return { nftAssetContract: nftAssetContractPrincipal, nftAssetId, tokenId: nftMintEvent.value.substr(1), block };
+}
+ 
+function mintFt({ chain, deployer, amount, recipient, paymentAssetContract = defaultPaymentAssetContract }: { chain: Chain, deployer: Account, amount: number, recipient: Account, paymentAssetContract?: string }) {
+    const block = chain.mineBlock([
+        Tx.contractCall(paymentAssetContract, 'mint', [types.uint(amount), types.principal(recipient.address)], deployer.address),
+    ]);
+    block.receipts[0].result.expectOk();
+    const ftMintEvent = block.receipts[0].events[0].ft_mint_event;
+    const [paymentAssetContractPrincipal, paymentAssetId] = ftMintEvent.asset_identifier.split('::');
+    return { paymentAssetContract: paymentAssetContractPrincipal, paymentAssetId, block };
+}
